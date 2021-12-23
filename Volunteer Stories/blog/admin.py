f5bba8fa40bc625f558e281a77729fdd892d3f7f
Meheraj <meherajmahmmd@gmail.com>
2021-12-24 02:52:17 +0600

    [FIXED] user creation, social share, newsletter, 2 latest posts

--- Volunteer Stories/blog/admin.py
+++ Volunteer Stories/blog/admin.py
@@ -3,7 +3,15 @@ from django.contrib import admin
 
 from blog.models import Category, Comment, Newsletter, Post
 
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ["email", "timestamp"]
+    list_display_links = ["email"]
+    list_filter = ["email"]
+    search_fields = ["email"]
+    list_per_page = 25
+
+
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Comment)
-admin.site.register(Newsletter)
+admin.site.register(Newsletter, NewsletterAdmin)
