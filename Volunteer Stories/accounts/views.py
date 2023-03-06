8c84f452639bab80db6b4fddaa98c7842fdf67be
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2023-03-06 13:36:14 +0600

    Update views.py

--- Volunteer Stories/accounts/views.py
+++ Volunteer Stories/accounts/views.py
@@ -1,72 +1,104 @@
-from django.shortcuts import redirect, render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, View
-
-from accounts.forms import AuthorForm, UserForm, UserUpdateForm, VolunteerJoinForm
-from accounts.models import Author
-from blog.models import Newsletter
-
-
-class AuthorCreateView(CreateView):
-    template_name = "accounts/author_form.html"
-    form_class = UserForm
-    success_url = reverse_lazy("login")
-
-    def form_valid(self, form):
-        user = form.save()
-        Author.objects.create(user=user)
-        Newsletter.objects.create(email=user.email)
-        return super().form_valid(form)
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_204_NO_CONTENT
+from rest_framework import views, viewsets, generics
+from rest_framework.viewsets import ModelViewSet
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
-class AuthorUpdateView(View):
-    def get(self, request, *args, **kwargs):
-        author_form = AuthorForm(instance=request.user.author)
-        user_form = UserUpdateForm(instance=request.user)
-        context = {"author_form": author_form, "user_form": user_form}
-        return render(request, "accounts/author_update.html", context=context)
-
-    def post(self, request, *args, **kwargs):
-        author_form = AuthorForm(
-            request.POST, request.FILES, instance=request.user.author
-        )
-        user_form = UserUpdateForm(request.POST, instance=request.user)
-        if author_form.is_valid() and user_form.is_valid():
-            user_form.save()
-            author_form.save()
-        return redirect("accounts_update")
+from .models import UserModel, ImageModel, VolunteerModel
+from .serializers import LoginSerializer, UserModelSerializer, LogoutSerializer, ImageModelSerializer, \
+    VolunteerSerializer
 
 
-def about_us_view(request):
-    page_name = "About"
-    context = {
-        "page_name": page_name,
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token)
     }
-    return render(request, "utility/about_us.html", context)
 
 
-def our_team_view(request):
-    return render(
-        request,
-        "utility/our_team.html",
-    )
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        data = request.data
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        if user:
+            user_data = UserModelSerializer.List(user).data
+            tokens = get_tokens_for_user(user)
+            return Response({'data': user_data, "tokens": tokens, }, status=HTTP_200_OK)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 
-def join_us_view(request):
-    page_name = "Join"
-    form = VolunteerJoinForm()
-    if request.method == "POST":
-        form = VolunteerJoinForm(request.POST)
-        print(form)
-        if form.is_valid():
-            form.save()
-            context = {
-                "page_name": page_name,
-                "submitted": True,
-            }
-            return render(request, "utility/volunteer_join.html", context)
-    context = {
-        "page_name": page_name,
-        "form": form,
-    }
-    return render(request, "utility/volunteer_join.html", context)
+class LogoutAPIView(views.APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = LogoutSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        refresh_token = serializer.validated_data['refresh_token']
+        try:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=HTTP_204_NO_CONTENT)
+        except Exception as e:
+            return Response({'detail': str(e)}, status=HTTP_400_BAD_REQUEST)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    http_method_names = ['get', 'head', 'options', 'post', 'put', 'patch', 'delete']
+    queryset = UserModel.objects.all()
+    search_fields = ['username', 'email']
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return UserModelSerializer.List
+        return UserModelSerializer.Write
+
+    def get_queryset(self):
+        if self.request.user.is_superuser or self.request.user.is_staff or self.request.user.is_admin:
+            return UserModel.objects.all()
+        return UserModel.objects.filter(id=self.request.user.id)
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.user.is_admin:
+            user = UserModel.objects.get(id=kwargs['pk'])
+            user_data = UserModelSerializer.List(user).data
+            return Response({'user': user_data}, status=HTTP_200_OK)
+        elif request.user.id == int(kwargs['pk']):
+            user = UserModel.objects.get(id=kwargs['pk'])
+            user_data = UserModelSerializer.List(user).data
+            return Response({'user': user_data}, status=HTTP_200_OK)
+        return Response({'message': 'You are not authorized to perform this action'}, status=HTTP_400_BAD_REQUEST)
+
+    def create(self, request, *args, **kwargs):
+        if request.user.is_admin:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            user = serializer.save()
+
+            user_data = UserModelSerializer.List(user).data
+            return Response({'user': user_data}, status=HTTP_200_OK)
+        return Response({'message': 'You are not authorized to perform this action'}, status=HTTP_400_BAD_REQUEST)
+
+
+class ImageModelViewSet(ModelViewSet):
+    queryset = ImageModel.objects.all()
+    serializer_class = ImageModelSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class VolunteerModelViewSet(ModelViewSet):
+    queryset = VolunteerModel.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return VolunteerSerializer.Write
+        return VolunteerSerializer.List
